35314d1e037f464632fa7b0475b8378ab41ef335
Stefdelec <s.delecroix@gmail.com>
2017-04-01 11:55:53 +0200

    change dbquestion

---
+++ dbquestion.js
@@ -0,0 +1,314 @@
+//questionListPleasure
+//questionListTec
+
+var questionListPleasure=[
+    {
+        question: "Combien d'étoiles ornent le drapeau chinois ? ",
+        answer: "5"
+    },
+    {
+        question: "Combien de fuseaux horaires traversent le Canada ? ",
+        answer: "Six",
+    },
+    {
+        question: "Combien de pays ont une frontière commune avec la France ?",
+        answer: "8"
+    },
+    {
+        question: "Comment appelle-t-on les habitants des Bouches-du-Rhône ?",
+        answer: " Les Bucco-rhodaniens"
+    },
+
+    {
+        question: "Comment s'appelait la première épouse de Napoléon Bonaparte ? ",
+        answer: "Joséphine de Beauharnais"
+    },
+    {
+        question: "Dans quel pays la dynastie Romanov fut-elle renversée par une révolution en 1917 ? ",
+        answer: " Russie"
+    },
+    {
+        question: "Dans quel pays se trouve Waterloo, où Napoléon a perdu la fameuse bataille de 1815 ? ",
+        answer: " En Belgique"
+    },
+    {
+        question: "Dans quel pays se trouve le dernier port d'escale du R.M.S. Titanic ?",
+        answer: " En Irlande"
+    },
+    {
+        question: "Dans quelle ville fut construit le tout premier métro du monde ?  ",
+        answer: " Londres"
+    },
+    {
+        question: "En quelle année fut créé le terme « Internet » ? ",
+        answer: " 1974"
+    },
+      {
+        question: "Combien de tableaux Van Gogh a-t-il vendus de son vivant ? ",
+        answer: " 1"
+    },
+      {
+        question: "Comment surnomme-t-on les membres de l'Académie française ?  ",
+        answer: " Les immortels (Duncan McLeod ???)"
+    },
+      {
+        question: "Au basketball, combien de joueurs une équipe peut-elle avoir à tout moment sur le terrain ? ",
+        answer: " Cinq"
+    },
+      {
+        question: "Au hockey, comment appelle-t-on le fait de marquer trois buts ?",
+        answer: " Un tour du chapeau"
+    },
+          {
+        question: "Quel est le pays dont le premier empereur repose dans un mausolée gardé par 8 000 guerriers de terre cuite ?  ",
+        answer: " La Chine"
+    },
+          {
+        question: "Quel fondateur d'une plate-forme de média social devint, en 2008, à l'âge de 23 ans, le plus jeune milliardaire du monde ?",
+        answer: " Mark Zuckerberg"
+    },
+          {
+        question: "Quel pont est le plus ancien de Paris ? ",
+        answer: " Le pont Neuf"
+    },
+          {
+        question: "En 1986, quel joueur de football a dit avoir reçu l'aide de Dieu pour marquer contre l'Angleterre lors des quarts de finale de la Coupe du monde ?",
+        answer: " Diego Maradona"
+    },
+          {
+        question: "En 2010, au cours de quel Grand Chelem eut lieu le plus long match de tennis du monde qui dura 11 heures ?",
+        answer: " Wimbledon"
+    },
+          {
+        question: "En 2013, quelle équipe LeBron James a-t-il menée aux finales NBA ?",
+        answer: " Les Heat de Miami"
+    },
+     {
+        question: "Quel est le composant dans la baguette d’Harry Potter ? ",
+        answer: "Une plume de phenix"
+    },
+    {
+        question: "Dans Doctor Who, comment s’appelle le vaisseau du Docteur ? ",
+        answer: "le Tardis"
+    },
+    {
+
+        question: "Que signifie TARDIS ? ",
+        answer: "VO : Time And Relative Dimension In Space - VF : Temps À Relativité Dimensionnelle Inter Spatiale"
+    },
+    {
+        question: "Qui sont Dick Grayson, Jason Todd et Tim Drake ? ",
+        answer: "Les 3 premiers Robin de Batman"
+    },
+    {
+        question: "Dans le film “Moulin Rouge”, quelle chanson de Police est reprise en version tango ?  ",
+        answer: "Roxanne"
+    },
+    {
+        question: "Quel est le plus long fleuve de France ?",
+        answer: "La Loire (~1012 km)"
+    },
+    {
+
+        question: "Terminez cette réplique \"Il dit qu'il a plus de genoux.\" ",
+        answer: "Il dit qu'il voit pas le rapport"
+    },
+    {
+        question: "Quel acteur joue dans les films Thor, Avengers, et Kong Skull Island ? ",
+        answer: "Tom Hiddleston"
+    },
+
+    {
+        question: "Qui sont les acteurs principaux du film “La La Land” ? ",
+        answer: "Ryan Gosling et Emma Stone"
+    },
+    {
+        question: "Qui a remporté le dernier match amical France-Espagne ? ",
+        answer: "Espagne (0-2)"
+    },
+    {
+        question: "Quelle est la capital du Boutan ?",
+        answer: "Thimphou"
+    },
+      {
+
+        question: "Comment s'appelle le pdg de Microsoft ?",
+        answer: "Satya Nadella "
+    },
+    {
+
+        question: "Comment s'appelle le pdg de Blackberry ?",
+        answer: "John S. Chen "
+    },
+
+]
+var questionListTec=questionListPleasure
+
+var archive1 = [
+    {
+        question: 'Que retourne l’expression (Johanna.color == “Red”)?“Pompette”:“Ça va”   ? ',
+        answer: "42"
+    },
+    {
+        question: "Quelle est la différence entre les delegates ACTION et FUNC ?",
+        answer: "Action n’a pas de retour alors que func en a un (comme le Brexit) "
+    },
+    {
+        question: "Que retourne l’expression suivante ? ",
+        answer: "true"
+    },
+    {
+        question: "Comment appelle-t-on le type d'écriture suivant : X => X.LENGTH == 12 ?",
+        answer: " Une expression lambda"
+    },
+
+    {
+        question: "Quel sera le résultat de cette opération : var result = 2/3 ? ",
+        answer: "Résultat de type int et de valeur 0"
+    },
+    {
+        question: "En C#, comment éviter le recours aux séquences d'échappement en affectant une valeur à une variable ? ",
+        answer: " En utilisant un @ comme : string s = @”Pas d’échappement. (ou en ne faisant pas de C#)"
+    },
+    {
+        question: "Quel est l’équivalent SQL Server de l’opérateur C# “??” ?",
+        answer: "la fonction ISNULL()"
+    },
+    {
+        question: "En css, que faire pour que mon texte soit en majuscule, que dois-écrire: text-transform:... ",
+        answer: "uppercase"
+    },
+ {
+        question: "En css, que fare pour que mon seul la première lettre de chaque mon de mon texte soit en majuscule, que dois-écrire: text-transform:... ",
+        answer: "capitalize"
+    },
+    {
+        question: "En ECMAScript 5, existe-t'il des Getters et des Setters pour JS ? ",
+        answer: "Seules les méthodes qui sont dans la même classe que la méthode en question"
+    },
+    {
+        question: " En quel language dois-je programmer pour faire une application dites native sur Android ? ",
+        answer: "JAVA "
+    },
+    {
+        question: "var arr2=arr1; var arr1=[0,1,2];  arr1[0]=5; Quelle est la valeur de arr2[0] ? ",
+        answer: "5"
+    },
+
+];
+
+var arch2 = [
+    {
+        question: "Quel est le composant dans la baguette d’Harry Potter ? ",
+        answer: "Une plume de phenix"
+    },
+    {
+        question: "Dans Doctor Who, comment s’appelle le vaisseau du Docteur ? ",
+        answer: "le Tardis"
+    },
+    {
+
+        question: "Que signifie TARDIS ? ",
+        answer: "VO : Time And Relative Dimension In Space - VF : Temps À Relativité Dimensionnelle Inter Spatiale"
+    },
+    {
+        question: "Qui sont Dick Grayson, Jason Todd et Tim Drake ? ",
+        answer: "Les 3 premiers Robin de Batman"
+    },
+    {
+        question: "Dans le film “Moulin Rouge”, quelle chanson de Police est reprise en version tango ?  ",
+        answer: "Roxanne"
+    },
+    {
+        question: "Quel est le plus long fleuve de France ?",
+        answer: "La Loire (~1012 km)"
+    },
+    {
+
+        question: "Terminez cette réplique \"Il dit qu'il a plus de genoux.\" ",
+        answer: "Il dit qu'il voit pas le rapport"
+    },
+    {
+        question: "Quel acteur joue dans les films Thor, Avengers, et Kong Skull Island ? ",
+        answer: "Tom Hiddleston"
+    },
+
+    {
+        question: "Qui sont les acteurs principaux du film “La La Land” ? ",
+        answer: "Ryan Gosling et Emma Stone"
+    },
+    {
+        question: "Qui a remporté le dernier match amical France-Espagne ? ",
+        answer: "Espagne (0-2)"
+    },
+    {
+        question: "Quelle est la capital du Boutan ?",
+        answer: "Thimphou"
+    },
+
+    {
+        question: "Quelle chanson commence par ces mots: 'Je marche vers les ténèbres Vers l'horizon funeste Mais la vie qui m'entoure et me baigne",
+        answer: "Bleu Noir de Mylène Farmer"
+    },
+    {
+        question: " Suite de lettres et de chiffres : 54 48 42 ? …… 30 24",
+        answer: "36"
+    },
+    {
+        question: "Comment appelle t'on un SSII aujourd'hui ? ",
+        answer: "ESN"
+    },
+    {
+        question: " Si je commence une phrase par \"j'éspère que...\" puis je terminer par un point d'interogation ?  ",
+        answer: "Non"
+    },
+    {
+        question: " Comment se prononce le nom de famille de cet IdiCollab : Nicolas Millon  ? ",
+        answer: "Il faut dire les 2 L"
+    },
+    {
+        question: " En quelle année est née MMA ?  ",
+        answer: "GENERAL"
+    },
+    {
+        question: " En France, selon Stackoverflow, quel language permet d'obtenir le meilleur salaire en moyenne ?  ",
+        answer: "Python"
+    },
+    {
+        question: " Reconstituer le mot manquant  : Domaine agricole / qui offre une certaine résistance à la pression …………………………… ( R M E E F )   ",
+        answer: "FERME"
+    },
+    {
+        question: " Reconstituer le mot manquant  : Militaire / saucisse sèche et plate …………………………………… ( E R E M N G D A ) ",
+        answer: "GENDARME"
+    },
+    {
+
+        question: " Qui a gagné le prix Goncourt 2016 ?",
+        answer: "Catherine Cusset "
+    },
+    {
+
+        question: "En quelle année a été crée Idigao ?",
+        answer: "04 novembre 2005 "
+    },
+    {
+
+        question: "Comment s'appelle le pdg de Microsoft ?",
+        answer: "Satya Nadella "
+    },
+    {
+
+        question: "Comment s'appelle le pdg de Blackberry ?",
+        answer: "John S. Chen "
+    },
+    {
+
+        question: "Comment s'appelle le candidat représentant les Verts à l'élection présidentiel ?",
+        answer: "Yannick Jadot"
+    },
+
+
+
+
+];
