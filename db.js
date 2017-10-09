a1582cf7d24c7f99866ffca2524894bc297a3d1d
stefdelec <s.delecroix@gmail.com>
2017-10-09 12:56:35 +0200

    Create db.js

---
+++ db.js
@@ -0,0 +1,17 @@
+const a=[
+{
+        question: "Combien y a t-il d'employées à Idigao ? ",
+        answer: "?",
+        author:"nca"
+    },
+    {
+        question: "Quelle technologie traitait la dernière Iditech ? ",
+        answer: "Angular",
+        author:"nca"
+    },
+    {
+        question: "Que symbolisent les cercles figurant sur le drapeau olympique ?",
+        answer: "Les cinq continents",
+        author:"nca"
+    }
+    ]
